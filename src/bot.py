8edd2f5e19e2ab2057d712607f075a7e8a9ea8b4
Nicholas Tung <nicholastung47@gmail.com>
2019-07-03 23:52:00 -0400

    Completed many things
    Minecraft commands
    fixed screech for both platforms hopefully

--- src/bot.py
+++ src/bot.py
@@ -1,13 +1,34 @@
+from datetime import datetime
 from discord.ext import commands
 import discord
 import config
-from datetime import datetime
+import json
+import urllib.request
 
 prefix = config.prefix
-bot = commands.Bot(command_prefix=prefix)
-bot.remove_command('help')
+bot = commands.Bot(command_prefix=prefix,
+                   help_command=None,
+                   activity=discord.Game("{}help".format(config.prefix)),
+                   owner_id=config.owner_id
+                   )
+
+# Helper Methods
 
 
+def getMCServerStatus(serverIP):
+    api = "https://api.mcsrvstat.us/2/{}".format(config.server)
+    with urllib.request.urlopen(api) as url:
+        return json.loads(url.read().decode())
+
+
+def standardEmbed(title, description):
+    embed = discord.Embed(color=0x43efeb)
+    embed.set_footer(text="tungbot-v4 | " +
+                     datetime.now().strftime("%B %d, %Y, %H:%M:%S"))
+    return embed
+
+
+# Bot Methods
 @bot.event
 async def on_ready():
     print("Everything's all ready to go~")
@@ -18,19 +39,102 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
+# @bot.event
+# async def on_command_error(error, ctx):
+#     if isinstance(error, commands.CommandOnCooldown):
+#         await ctx.send("This command is on a %.2f second cooldown" % error.retry_after)
+
+#     raise error
+
+
 @bot.group()
 async def server(ctx):
-    pass
+    """Group of commands to query the minecraft server"""
+    if ctx.invoked_subcommand is None:
+        await ctx.send('Invalid server command passed.')
 
 
 @server.command()
+@commands.cooldown(15, 60, commands.BucketType.guild)
 async def status(ctx):
-    pass
+    """Get server status"""
+
+    data = getMCServerStatus(config.server)
+
+    title = "Server {}".format("Online" if data["online"] else "Offline")
+    description = "Basic server status for minecraft server"
+
+    embed = standardEmbed(title, description)
+    embed.set_thumbnail(
+        url="https://gamepedia.cursecdn.com/minecraft_gamepedia/4/44/Grass_Block_Revision_6.png"
+    )
+    embed.add_field(name="URL", value="{}:{}".format(config.server, data["port"]))
+
+    if data["online"]:
+        embed.add_field(name="MOTD", value=data["motd"]["raw"])
+        embed.add_field(name="Version", value=data["version"])
+        embed.add_field(name="Software", value=data["software"])
+        embed.add_field(
+            name="Capacity",
+            value="{}/{}".format(
+                data["players"]["online"],
+                data["players"]["max"]
+            )
+        )
+
+    await ctx.send(embed=embed)
 
 
 @server.command()
-async def players():
-    pass
+@commands.cooldown(15, 60, commands.BucketType.guild)
+async def players(ctx):
+    """Get online players"""
+
+    data = getMCServerStatus(config.server)
+
+    title = "Players Online"
+    description = "List of players online"
+    embed = standardEmbed(title, description)
+    embed.set_thumbnail(
+        url="https://gamepedia.cursecdn.com/minecraft_gamepedia/4/44/Grass_Block_Revision_6.png"
+    )
+
+    if not data["online"]:
+        embed.add_field(name="Server Offline")
+        return
+    else:
+        embed.add_field(
+            name="Capacity",
+            value="{}/{}".format(
+                data["players"]["online"],
+                data["players"]["max"]
+            )
+        )
+
+        await ctx.send(embed=embed)
+
+        for player in list(data["players"]["list"]):
+            title = "Player"
+            description = ""
+            playerEmbed = standardEmbed(title, description)
+            
+            playerEmbed.add_field(name="Player", value=player)
+            playerEmbed.set_thumbnail(
+                url="https://minotar.net/avatar/{}/300.png".format(player)
+            )
+            await ctx.send(embed=playerEmbed)
+
+
+@bot.command()
+async def shutdown(ctx):
+    """For owners only: shutdown the bot"""
+    yes = await bot.is_owner(ctx.message.author)
+    if yes:
+        await ctx.send("Successfully shutting down")
+        await bot.close()
+    else:
+        await ctx.send("You cannot shut down this bot")
+
 
 @bot.command()
 async def screech(ctx):
@@ -44,16 +148,15 @@ async def screech(ctx):
 
     channel = ctx.message.author.voice.channel
 
-    await channel.connect()
+    voice = await channel.connect()
 
     source = discord.PCMVolumeTransformer(
         discord.FFmpegPCMAudio("assets/screech.mp3"))
-    await ctx.voice_client.play(source, after=lambda e: print(
-        'Player error: %s' % e) if e else None)
+    await voice.play(source)
 
     await ctx.send('Now screeching!')
 
-    await ctx.voice_client.disconnect()
+    await voice.disconnect()
 
 
 @bot.command()
@@ -65,13 +168,14 @@ async def stop(ctx):
 
 
 @bot.command()
-async def help(ctx):
+async def help(ctx, group=""):
     """Lists all bot functions"""
 
     embed = discord.Embed(
         title="Help",
         description="All commands explained here!",
-        color=0x43efeb)
+        color=0x43efeb
+    )
 
     embed.set_author(name="tungbot",
                      url="https://github.com/st0p47/tungbot-v4",
@@ -80,8 +184,13 @@ async def help(ctx):
     embed.set_footer(text="tungbot-v4 | " +
                      datetime.now().strftime("%B %d, %Y, %H:%M:%S"))
 
-    for command in bot.commands:
-        embed.add_field(name=command.name, value=command.short_doc)
+    # Super hacky because I got lazy
+    if group == "server":
+        for command in server.commands:
+            embed.add_field(name=command.name, value=command.short_doc, inline=False)
+    else:
+        for command in bot.commands:
+            embed.add_field(name=command.name, value=command.short_doc, inline=False)
 
     await ctx.send(embed=embed)
 
